a4805037f0ce4dc05cf15b3edb9e5c0490762d19
Andrea <a_andrea.lozano.mercado@iespablopicasso.es>
2023-05-25 19:36:06 +0200

    Método para contar el número de veces añadido

--- Act4eGit_Andrea/src/numerosAleatorios.java
+++ Act4eGit_Andrea/src/numerosAleatorios.java
@@ -17,4 +17,19 @@ public static void imprimirArray(int[] array) {
     System.out.println(); 
 }
 
+public static void contarNumeros(int[] array) {
+    int[] contador = new int[11]; // Utilizamos un array de tamaño 11 para almacenar el contador de cada número (índice 0 a 10)
+
+    // Contar ocurrencias de cada número en el array
+    for (int num : array) {
+        contador[num]++;
+    }
+
+    // Mostrar el resultado por pantalla
+    System.out.println("Conteo de ocurrencias de números:");
+    for (int i = 1; i <= 10; i++) {
+        System.out.println("Número " + i + ": " + contador[i] + " veces");
+    }
+}
+
 }
